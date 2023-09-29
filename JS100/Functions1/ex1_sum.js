fcf48194e7574ad11d2ab1a3a5ad81ed33660a00
Iván E. Hernández T <iamieht@gmail.com>
2023-09-29 12:30:46 +0200

    Create ex1_sum.js
    Functions1 - ex1

---
+++ JS100/Functions1/ex1_sum.js
@@ -0,0 +1,10 @@
+// Problem:
+// Write a function sum that accepts 2 arguments and returns the sum of its arguments
+
+// Test Case / Examples
+console.log(sum(22, 10));
+
+// Code
+function sum(oper1, oper2) {
+  return oper1 + oper2;
+}
