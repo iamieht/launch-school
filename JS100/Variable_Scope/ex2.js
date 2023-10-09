d820beb0e90e6b42e9d50143ce51eb7526f1ade1
Iván E. Hernández T <iamieht@gmail.com>
2023-10-09 07:44:44 +0200

    Create ex2.js
    Variable Scope - Ex2

---
+++ JS100/Variable_Scope/ex2.js
@@ -0,0 +1,9 @@
+// What's my value? (Part 2)
+
+console.log(greeting);
+
+let greeting = "Hello world!";
+
+// Solution
+// ReferenceError
+// In contrast to var variables, let variables are not accessible before they are declared. For that reason the above code raises an error.
