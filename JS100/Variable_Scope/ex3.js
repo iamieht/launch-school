b2984ac4ebd705434cc8234b75e2764008bd99af
Iván E. Hernández T <iamieht@gmail.com>
2023-10-09 07:49:19 +0200

    Create ex3.js
    Variable Scope - ex3

---
+++ JS100/Variable_Scope/ex3.js
@@ -0,0 +1,10 @@
+// What's my value? (Part 3)
+if (true) {
+  let myValue = 20;
+}
+
+console.log(myValue);
+
+// Solution
+// ReferenceError: myValue is not defined
+// Variables declared with let are block scoped. This means that when we declare the variable myValue within a block on line 2, that variable is not accessible outside of the block on line 5, and a ReferenceError is raised.
