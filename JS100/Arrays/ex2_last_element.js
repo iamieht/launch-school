debf4391b2d777d6cf69d187ccad2dd622e3d686
Ivan E Hernandez T <iamieht@gmail.com>
2023-10-10 18:18:55 +0200

    Arrays - ex2

---
+++ JS100/Arrays/ex2_last_element.js
@@ -0,0 +1,8 @@
+// Write a function that returns the last element of an input array.
+function last(array) {
+  return array[array.length - 1];
+}
+
+// Example
+
+console.log(last(["Earth", "Moon", "Mars"])); // 'Mars'
