de92516864d5311c08b8cbe309a84ce510067934
Ivan E Hernandez T <iamieht@gmail.com>
2023-10-10 18:13:59 +0200

    Arrays - ex1

---
+++ JS100/Arrays/ex1_first_element.js
@@ -0,0 +1,7 @@
+// Write a function that returns the first element of an input array.
+function first(array) {
+  return array[0];
+}
+
+// Example
+console.log(first(["Earth", "Moon", "Mars"])); // 'Earth'
