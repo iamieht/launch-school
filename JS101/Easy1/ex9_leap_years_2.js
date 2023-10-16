7923444f9e421e315a8e23355cbeac27868f02b2
Iván E. Hernández T <iamieht@gmail.com>
2023-10-16 16:20:03 +0200

    Create ex9_leap_years_2.js
    Wasy1 - Ex9 - Leap Years part 2

---
+++ JS101/Easy1/ex9_leap_years_2.js
@@ -0,0 +1,59 @@
+// Problem
+// Create a funtion that returns true if a year is a leap year, false otherwise
+
+// Input
+// - Year: Number
+
+// Output
+// - Boolean
+
+// Explicit rules
+// - If the year is <= 1752 any year evenly divisible by 4 is a Leap Year.
+// - If year is evenly divisible by 4 and 400 is a leap year
+// - If a year is evenly divisible by 100: Not a leap year
+// - If a year is evenly divisible by 100 and 400: leap year
+// - Rule valid for any year greater than 0.
+
+// Implicit rules
+//
+
+// Examples / Test Cases
+console.log(isLeapYear(2016)); // true
+console.log(isLeapYear(2015)); // false
+console.log(isLeapYear(2100)); // false
+console.log(isLeapYear(2400)); // true
+console.log(isLeapYear(240000)); // true
+console.log(isLeapYear(240001)); // false
+console.log(isLeapYear(2000)); // true
+console.log(isLeapYear(1900)); // false
+console.log(isLeapYear(1752)); // true
+console.log(isLeapYear(1700)); // true
+console.log(isLeapYear(1)); // false
+console.log(isLeapYear(100)); // true
+console.log(isLeapYear(400)); // true
+
+// Data Structures
+// - Number
+// - Boolean
+
+// Algorithm
+// - Declare a function isLeapYear(year)
+// - If year is <= 1752 is a leap year if evenly divisible by 4
+// - Create an if condition to check:
+//    - Year is evenly divisible by 100 and 400: => return true
+//    - Year is evenly divisible by 4 and 100: => return false
+//    - Year is evenly divisible by 100: => return false
+//    - Year is evenly divisible by 4: => return true
+
+// Code
+function isLeapYear(year) {
+  if (year <= 1752) {
+    return year % 4 === 0;
+  } else if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else {
+    return year % 4 === 0;
+  }
+}
