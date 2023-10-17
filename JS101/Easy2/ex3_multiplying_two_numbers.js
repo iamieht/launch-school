b529b54b80832896941aaf75b698b461dc5a54d3
Iván E. Hernández T <iamieht@gmail.com>
2023-10-17 16:42:38 +0200

    Create ex3_multiplying_two_numbers.js
    Easy2 - Ex3 - Multiply

---
+++ JS101/Easy2/ex3_multiplying_two_numbers.js
@@ -0,0 +1,24 @@
+// Problem
+// Create a function that takes two arguments, multiplies them together, and returns the result.
+
+// Input
+// - Number
+// - Number
+
+// Output
+// - Number
+
+// Examples / Test Case
+console.log(multiply(5, 3) === 15); // logs true
+
+// Data Structures
+// - Number
+
+// Algorithm
+// - Declare a function multiply(num1, num2)
+// - Return num1 * num2
+
+// Code
+function multiply(num1, num2) {
+  return num1 * num2;
+}
