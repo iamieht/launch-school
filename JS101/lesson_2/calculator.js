fc7fd5f16b267ef304f8a556112026de9a863843
Iván E. Hernández T <iamieht@gmail.com>
2023-11-20 09:15:30 +0100

    Create calculator.js
    lesson 2 first version calculator

---
+++ JS101/lesson_2/calculator.js
@@ -0,0 +1,30 @@
+// Welcome
+console.log('Welcome to Calculator!');
+const READLINE = require('readline-sync');
+
+// Ask the user for the first number
+let firstNumber = Number(READLINE.question('Introduce first number: '));
+
+// Ask the user for the second number
+let secondNumber = Number(READLINE.question('Introduce second number: '));
+
+// Ask the user for an operation to perform
+let operation = READLINE.question(
+  'Introduce arithmetic operation\n1) Add 2) Subtract 3) Multiply 4) Divide: '
+);
+
+// Perform the operation on the two numbers and Print the result to the terminal
+switch (operation) {
+  case '1':
+    console.log(firstNumber + secondNumber);
+    break;
+  case '2':
+    console.log(firstNumber - secondNumber);
+    break;
+  case '3':
+    console.log(firstNumber * secondNumber);
+    break;
+  case '4':
+    console.log(firstNumber / secondNumber);
+    break;
+}
