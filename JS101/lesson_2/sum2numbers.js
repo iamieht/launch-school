6ad779d830745d31cf500ecc6df0fb841e872538
Iván E. Hernández T <iamieht@gmail.com>
2023-11-20 14:00:33 +0100

    Create sum2numbers.js
    a function that returns the sum of two numbers

---
+++ JS101/lesson_2/sum2numbers.js
@@ -0,0 +1,26 @@
+// Casual pseudocode
+// a function that returns the sum of two numbers
+
+// Declare a function sum with two parameters, each representing a number
+// Declare a variable to store the result of the sum
+// Add the two numbers
+// return the result
+
+// Formal pseudocode
+// START
+
+// a function that returns the sum of two numbers
+
+// SET sum(num1, num2)
+// SET result
+// result = num1 + num2
+// PRINT result
+
+// Code
+function sum(num1, num2) {
+  let result = num1 + num2;
+  return result;
+}
+
+// Examples
+console.log(sum(5, 5));
