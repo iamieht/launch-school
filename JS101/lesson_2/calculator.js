2f7071f54a5f1c4159d120595c552fbbdcb89cdd
Iván E. Hernández T <iamieht@gmail.com>
2023-11-21 11:47:37 +0100

    Update calculator.js
    Refactor caclulator

--- JS101/lesson_2/calculator.js
+++ JS101/lesson_2/calculator.js
@@ -1,32 +1,59 @@
 // Welcome
-console.log('Welcome to Calculator!');
 const READLINE = require('readline-sync');
 
+function prompt(msg) {
+  console.log(`=> ${msg}`);
+}
+
+function invalidNumber(num) {
+  return num.trimStart() === '' || Number.isNaN(Number(num));
+}
+prompt('Welcome to Calculator!');
+
 // Ask the user for the first number
-let firstNumber = Number(READLINE.question('Introduce first number: '));
+prompt('What is the first number?');
+let firstNumber = READLINE.question();
+
+while (invalidNumber(firstNumber)) {
+  prompt("Hmmm... that doesn't look like a valid number.");
+  firstNumber = READLINE.question();
+}
 
 // Ask the user for the second number
-let secondNumber = Number(READLINE.question('Introduce second number: '));
+prompt('What is the second number?');
+let secondNumber = READLINE.question();
+
+while (invalidNumber(secondNumber)) {
+  prompt("Hmmm... that doesn't look like a valid number.");
+  secondNumber = READLINE.question();
+}
 
 // Ask the user for an operation to perform
-let operation = READLINE.question(
-  'Introduce arithmetic operation\n1) Add 2) Subtract 3) Multiply 4) Divide: '
+prompt(
+  'What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide'
 );
+let operation = READLINE.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3 or 4.');
+  operation = READLINE.question();
+}
 
 // Perform the operation on the two numbers and Print the result to the terminal
+let output;
 switch (operation) {
   case '1':
-    console.log(firstNumber + secondNumber);
+    output = Number(firstNumber) + Number(secondNumber);
     break;
   case '2':
-    console.log(firstNumber - secondNumber);
+    output = Number(firstNumber) - Number(secondNumber);
     break;
   case '3':
-    console.log(firstNumber * secondNumber);
+    output = Number(firstNumber) * Number(secondNumber);
     break;
   case '4':
-    console.log(firstNumber / secondNumber);
+    output = Number(firstNumber) / Number(secondNumber);
     break;
-  default:
-    console.log('Invalid input!');
 }
+
+prompt(`The result is ${output}.`);
