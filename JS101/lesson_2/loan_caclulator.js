0f3114cca7e553fb3f5db6587bae56d43a27ed52
Iván E. Hernández T <iamieht@gmail.com>
2023-11-23 13:47:09 +0100

    Update loan_caclulator.js
    Move calculation to a function and validate inputs

--- JS101/lesson_2/loan_caclulator.js
+++ JS101/lesson_2/loan_caclulator.js
@@ -4,27 +4,53 @@ function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
+function invalidNumber(num) {
+  return num.trimStart() === '' || Number.isNaN(Number(num));
+}
+
+function calculateLoan(loanAmount, monthlyInterestRate, loanDurationInMonths) {
+  let monthlyPayment =
+  Number(loanAmount) *
+  (monthlyInterestRate /
+    (1 - Math.pow(1 + monthlyInterestRate, -Number(loanDurationInMonths))));
+
+  return monthlyPayment;
+}
+
 prompt('Welcome to the Loan Calculator!');
 
-// Ask user for loan amount
+// Ask user for loan amount and validate input
 prompt('What is the loan amount?');
 let loanAmount = READLINE.question();
 
-// Ask the user for the loan term (months)
+while (invalidNumber(loanAmount)) {
+  prompt("Hmmm... that doesn't look like a valid number.");
+  loanAmount = READLINE.question();
+}
+
+// Ask the user for the loan term (months) and validate input
 prompt('What is the loan term in months?');
 let loanDurationInMonths = READLINE.question();
+
+while (invalidNumber(loanDurationInMonths)) {
+  prompt("Hmmm... that doesn't look like a valid number.");
+  loanDurationInMonths = READLINE.question();
+}
 
 // Ask the user for the Annual Percentage Rate (APR) in percentage
 prompt('What is the Interest Rate (In %)?');
 let annualInterestRate = READLINE.question();
 
+while (invalidNumber(annualInterestRate)) {
+  prompt("Hmmm... that doesn't look like a valid number.");
+  annualInterestRate = READLINE.question();
+}
+
 // Do the loan calculation
 let monthlyInterestRate = Number(annualInterestRate) / 100 / 12;
 
-let monthlyPayment =
-  Number(loanAmount) *
-  (monthlyInterestRate /
-    (1 - Math.pow(1 + monthlyInterestRate, -Number(loanDurationInMonths))));
+let monthlyPayment = calculateLoan(loanAmount,
+  monthlyInterestRate, loanDurationInMonths);
 
 let totalPayments = monthlyPayment * loanDurationInMonths;
 let totalInterest = (monthlyPayment * loanDurationInMonths) - loanAmount;
@@ -39,3 +65,4 @@ console.log(`Total Interest: ${totalInterest.toFixed(2)}`);
 // TODO
 // - Validate inputs
 // - 0% interest rate
+// - Move calculation to a function [X]
