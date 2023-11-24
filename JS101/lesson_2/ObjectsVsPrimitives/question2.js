c70ac555ffec62f0b18cc081ac445c63b75f508f
Iván E. Hernández T <iamieht@gmail.com>
2023-11-24 10:29:49 +0100

    Create question2.js
    Objects vs Primitives: question 2

---
+++ JS101/lesson_2/ObjectsVsPrimitives/question2.js
@@ -0,0 +1,7 @@
+let myWord = 'Hello';
+myWord.repeat(3);
+console.log(myWord); // => 'Hello'
+myWord.replace('H', 'J');
+console.log(myWord); // => 'Hello'
+myWord.toUpperCase();
+console.log(myWord); // => 'Hello'
