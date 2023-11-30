c9d55693f62e760631b9eaddbe60eba7199aab73
Ivan E. Hernandez T. <iamieht@gmail.com>
2023-11-30 18:20:27 +0100

    Easy3 - Question5

---
+++ JS101/lesson_3/Easy3/q5.js
@@ -0,0 +1,14 @@
+// function isColorValid(color) {
+//   if (color === "blue" || color === "green") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+function isColorValid(color) {
+  return color === 'blue' || color === 'green';
+}
+
+console.log(isColorValid('blue'));
+console.log(isColorValid('black'));
